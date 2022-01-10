a3b338b6e52f2fa1429223b9310b1f399985b35f
Canh Tran <7752679+canhth@users.noreply.github.com>
2022-01-10 14:17:36 +0700

    Add files via upload

---
+++ acextract.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Acextract < Formula
+  desc "A tool to list and extract content from Assets.car file."
+  homepage "https://github.com/bartoszj/acextract"
+  url "https://github.com/canhth/acextract/archive/refs/tags/2.3.tar.gz"
+  sha256 "889e666badb6a53523b87620b34354e8e3fd13bac876f3dbe5718b6550bc268b"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    bin.install 'acextract'
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test acextract`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
